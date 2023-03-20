28399995e2184b98a8428b10f8114d74ba351348
Ashraf Mollah <mollah.dev@gmail.com>
2023-03-20 08:52:36 +0600

    added tailwind and font family

---
+++ src/components/HeaderNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>header component</h2>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HeaderNavigation",
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import "@/assets/scss/fonts.scss";
+import "@/assets/scss/tailwind.scss";
 
 createApp(App).use(store).use(router).mount("#app");
